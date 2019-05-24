f3fa4430a0a28af43a1fd177bc9c253862f60023
Ivan Aranda <IvanAranda@iMac.local>
2019-05-24 19:15:27 -0400

    edited couple of things

--- src/StopWatchRunner.java
+++ src/StopWatchRunner.java
@@ -13,31 +13,29 @@ import java.util.TimerTask;
  */
 public class StopWatchRunner extends JLabel implements MouseListener {
 	private static Timer timer;
-	private static TimerTask timerTask;
-	protected static StopWatchRunner obj; 
 	private int t;
-   private long startTime;   // Start time of timer.
-                             //   (Time is measured in milliseconds.)
+	private long startTime;   // Start time of timer.
+ //   (Time is measured in milliseconds.)
 
-   private boolean running;  // True when the timer is running.
+	private boolean running;  // True when the timer is running.
 
    /**
-    * Constructor sets initial text on the label to
-    * "Click to start timer." and sets up a mouse listener
-    * so the label can respond to clicks.
-    */
-   public StopWatchRunner() {
+* Constructor sets initial text on the label to
+* "Click to start timer." and sets up a mouse listener
+* so the label can respond to clicks.
+*/
+	public StopWatchRunner() {
       super("  Click to start timer.  ", JLabel.CENTER);
       addMouseListener(this);
    }
    
    
    /**
-    * Tells whether the timer is currently running.
-    */
-   public boolean isRunning() {
+* Tells whether the timer is currently running.
+*/
+	public boolean isRunning() {
       return running;
-   }
+	}
 
    
    /**
@@ -45,40 +43,40 @@ public class StopWatchRunner extends JLabel implements MouseListener {
     * or stopping the timer and changing the text that
     * is shown on the label.
     */
-   public void mousePressed(MouseEvent evt) {
-	  
-      if (running == false) {
-    	 t = 1;
-         running = true;
-         startTime = evt.getWhen();  // Time when mouse was clicked.
-         setText("Timing....");
-         timer = new Timer();
-         TimerTask task = new TimerTask() {
-             @Override
-             public void run() {
-            	 setText(t++ +"secs");
-             }
-         };
-         
-         timer.scheduleAtFixedRate(task, 1000, 1000);
-            
-      }
-      else {
-    	  
-            // Stop the timer.  Compute the elapsed time since the
-            // timer was started and display it.
-         running = false;
-         long endTime = evt.getWhen();
-         double seconds = (endTime - startTime) / 1000.0;
-         setText("Time: " + seconds + " sec.");
-         timer.cancel();
-         timer.purge();
-      }
-   }
-
-   public void mouseReleased(MouseEvent evt) { }
-   public void mouseClicked(MouseEvent evt) { }
-   public void mouseEntered(MouseEvent evt) { }
-   public void mouseExited(MouseEvent evt) { }
+	   public void mousePressed(MouseEvent evt) {
+		  
+	      if (running == false) {
+	    	 t = 1;
+	         running = true;
+	         startTime = evt.getWhen();  // Time when mouse was clicked.
+	         setText("Timing....");
+	         timer = new Timer();
+	         TimerTask task = new TimerTask() {
+	             @Override
+	             public void run() {
+	            	 setText(t++ +"secs");
+	             }
+	         };
+	         
+	         timer.scheduleAtFixedRate(task, 1000, 1000);
+	            
+	      }
+	      else {
+	    	  
+	            // Stop the timer.  Compute the elapsed time since the
+	            // timer was started and display it.
+	         running = false;
+	         long endTime = evt.getWhen();
+	         double seconds = (endTime - startTime) / 1000.0;
+	         setText("Time: " + seconds + " sec.");
+	         timer.cancel();
+	         timer.purge();
+	      }
+	   }
+	
+	   public void mouseReleased(MouseEvent evt) { }
+	   public void mouseClicked(MouseEvent evt) { }
+	   public void mouseEntered(MouseEvent evt) { }
+	   public void mouseExited(MouseEvent evt) { }
 
 }
